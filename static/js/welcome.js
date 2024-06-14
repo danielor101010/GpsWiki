a49a9cec5305c375020a6242c3f1a215eaab284c
danielor101010 <77803609+danielor101010@users.noreply.github.com>
2024-06-14 17:45:36 +0300

    change welcome buttons

--- static/js/welcome.js
+++ static/js/welcome.js
@@ -1,10 +1,4 @@
 function startApp() {
-    // Check if user is authenticated (example: check if user token exists)
-    if (localStorage.getItem('token')) {
-        // Redirect to main page
+
         window.location.href = '/main';
-    } else {
-        // Redirect to login page if not authenticated
-        window.location.href = '/main';
-    }
 }
